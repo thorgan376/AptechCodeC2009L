1f298284306c19e337d73c0572ab81d81de1a14b
hoangthaison393 <son.ht.909@aptechlearning.edu.vn>
2021-10-13 21:44:16 +0700

    update

---
+++ AP/13-10-2021/EpcProject/EpcProject/Fibonaci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpcProject
+{
+    public class Fibonaci
+    {
+        private int n;
+        public int N { get => n; 
+            set {
+                if (value < 2) {
+                    throw new Exception("N must be >= 2");
+                }
+                n = value;
+            } 
+        }
+        public void Calculate()
+        {
+            double f0 = 1;
+            double f1 = 1;
+            double f2;
+            for(int i = 2; i <=N; i++)
+            {
+                f2 = f0 + f1;
+                f0 = f1;
+                f1 = f2;
+                Console.WriteLine($"f({i}) = {f2}");
+            }
+        }
+           
+    }
+}
---
+++ AP/13-10-2021/EpcProject/EpcProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EpcProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonaci fibonaci = new Fibonaci()
+            {
+                N = 1000
+            };
+            fibonaci.Calculate();
+        }
+    }
+}
