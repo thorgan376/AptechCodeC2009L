40b805c42be0a00856288328921cbada07d64eca
thorgan376 <son.ht.909@aptechlearning.edu.vn>
2021-11-04 21:38:40 +0700

    update

---
+++ WPFC-WPF Course/01-11-2021/MyApp/MyApp/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using MyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Repositories
+{
+    public class UserRepository
+    {
+        public Student Login(String username, String password)
+        {
+            try {                
+                SqlConnection connection = Database.getInstance().GetConnection();                
+                string sql = @"SELECT * FROM tblStudent WHERE UserNm = @username AND Password=@password";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}", reader[0]));
+                    Student student = new Student();
+                    //student.Id = 
+                    //debug here
+                    return student;                    
+                }
+                return null;
+            }
+            catch (Exception e) {
+                throw e;                                
+            }
+        }
+    }
+}
---
+++ WPFC-WPF Course/03-11-2021/MyApp/MyApp/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using MyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Repositories
+{
+    public class UserRepository
+    {
+        public Student Login(String username, String password)
+        {
+            try {                
+                SqlConnection connection = Database.getInstance().GetConnection();                      
+                string sql = @"SELECT * FROM tblStudent WHERE UserNm = @username AND Password=@password";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}", reader[0]));
+                    Student student = new Student();                    
+                    student.Id = Convert.ToInt32(reader[0]);
+                    student.Name = Convert.ToString(reader[1]);
+                    student.Gender = Convert.ToString(reader[2]);
+                    student.DateOfBirth = Convert.ToDateTime(reader[3]);
+                    student.Address = Convert.ToString(reader[4]);
+                    student.UserName = Convert.ToString(reader[6]);                    
+                    //debug here
+                    return student;                    
+                }
+                return null;
+            }
+            catch (Exception e) {
+                throw e;                                
+            }
+        }
+    }
+}
