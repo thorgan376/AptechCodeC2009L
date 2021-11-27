976711937cbfc72ef124fe8f86a4c02cf998fec9
hoangthaison393 <son.ht.909@aptechlearning.edu.vn>
2021-11-28 01:04:36 +0700

    update

--- WPFC-WPF Course/05-11-2021/MyApp/MyApp/Database/DatabaseConn.cs
+++ WPFC-WPF Course/05-11-2021/MyApp/MyApp/Database/DatabaseConn.cs
@@ -16,11 +16,11 @@ namespace MyApp.Database
         B3: Trong dòng Data Source, đổi Microsoft SQL Sever thành Microsoft SQL Sever Database File
         B4: Trong dòng Database File Name (new or existing), tìm đến và chọn file .mdf vừa tạo trong thư mục này
         B5: Cuối cùng, thêm new query và chạy phần CODE SQL có sẵn,
-        And Run this app -You are done ~ NICE- */
+        And Run this app -You are done ~ NICE- và nếu có lỗi thêm thì thêm vào Connection String  "MultipleActiveResultSet=True"*/
 
-        private const string CONNECTIONSTRING = @"Data Source=(LocalDB)\MSSQLLocalDB;"+
+        private const string CONNECTIONSTRING = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
             @"AttachDbFilename=C:\Users\Thai Son\Desktop\Aptech\AptechCodeC2009L\AptechCodeC2009L\WPFC-WPF Course\05-11-2021\MyApp\MyApp\Database\Database.mdf;"+
-            @"Integrated Security=True;Connect Timeout=30;MultipleActiveResultSets=True;";
+            @"Integrated Security=True;Connect Timeout= 30; MultipleActiveResultSets=True;";
         private SqlConnection conn = null;
         //singleton object
         private static DatabaseConn instance;
@@ -47,7 +47,7 @@ namespace MyApp.Database
                 }
                 conn = new SqlConnection(CONNECTIONSTRING);
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("USE master", conn);
+                SqlCommand cmd = new SqlCommand("Select * from tblClass;", conn);
                 cmd.ExecuteReader();
                 cmd.Dispose();
                 return conn;
