10b96d3506800688b0f050076263b63b9642fcf3
thorgan376 <son.ht.909@aptechlearning.edu.vn>
2022-01-14 20:05:29 +0700

    update

---
+++ MVC_ASP.NET/08-12-2021/MyWebApp/MyWebApp/Controllers/HumanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class HumanController : Controller
+    {
+        // GET: Human
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_ASP.NET/WAD_C2009L_HoangThaiSon/WAD_C2009L_HoangThaiSon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WAD_C2009L_HoangThaiSon.Startup))]
+namespace WAD_C2009L_HoangThaiSon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
