bfa72f7efe83364b231e182dcb8cf4df6b4e3396
thorgan376 <son.ht.909@aptechlearning.edu.vn>
2022-01-19 17:37:25 +0700

    update

---
+++ MVC_ASP.NET/WAD_HOANGTHAISON_C2009L/WAD_HOANGTHAISON_C2009L/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WAD_HOANGTHAISON_C2009L.MvcApplication" Language="C#" %>
---
+++ MVC_ASP.NET/WAD_HOANGTHAISON_C2009L/WAD_HOANGTHAISON_C2009L/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WAD_HOANGTHAISON_C2009L.Startup))]
+namespace WAD_HOANGTHAISON_C2009L
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
